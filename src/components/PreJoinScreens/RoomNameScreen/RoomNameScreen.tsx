12b53bbb68de21a2da144915d85b68ba102f4469
hei-latticespace <hei@latticespace.com>
2022-01-20 11:51:25 +0800

    update
    update roonamescreen

--- src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
+++ src/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
@@ -52,43 +52,52 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
 
   const hasUsername = !window.location.search.includes('customIdentity=true') && user?.displayName;
 
-  const query = new URLSearchParams(window.location.search);
-  console.log('Room Name Screen ....');
-  console.log(window.sessionStorage.getItem('passcode'));
-  console.log(window.sessionStorage.getItem('roomname'));
-  console.log(window.sessionStorage.getItem('username'));
-
-  name = window.sessionStorage.getItem('username') as string;
-  if (name != '') setName(name);
-  roomName = window.sessionStorage.getItem('roomname') as string;
-  if (roomName != '') setRoomName(roomName);
-
   let form = useRef(null);
-  console.log(form);
-  console.log(form.current);
-  if (form.current !== null) {
-    console.log('yes');
-    //form.current.submit();
-  }
   useEffect(() => {
-    console.log('use effect');
+    console.clear();
+    console.log('------ use effect -------');
     console.log(form.current);
     //form.current.submit();
+
+    const query = new URLSearchParams(window.location.search);
+    console.log('Room Name Screen ....');
+    console.log('Storage Room Name : ' + window.sessionStorage.getItem('roomname'));
+    console.log('Storage User Name : ' + window.sessionStorage.getItem('username'));
+
+    name = window.sessionStorage.getItem('username') as string;
+    if (name != '') setName(name);
+    roomName = window.sessionStorage.getItem('roomname') as string;
+    if (roomName != '') setRoomName(roomName);
+
+    if (name != '' && roomName != '') {
+      console.log('DO FORM SUBMIT...');
+      //document.forms[0].submit();
+      //document.forms[0].elements["continue-button"].click();
+      //document.forms[0].elements[4].click();
+      //let element: HTMLElement = document.getElementById('continue-button')[0] as HTMLElement;
+      //element.click();
+      let element: HTMLElement = document.getElementById('continue-button') as HTMLElement;
+      console.log(element);
+      setTimeout(() => {
+        console.log('Click');
+        element.click();
+      }, 300);
+    }
   }, [form]);
 
   const disabled = true;
 
   return (
     <>
-      <Typography variant="h5" className={classes.gutterBottom}>
+      <Typography variant="h5" className={classes.gutterBottom} style={{ display: 'none' }}>
         Join a Room
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" style={{ display: 'none' }}>
         {hasUsername
           ? "Enter the name of a room you'd like to join."
           : "Enter your name and the name of a room you'd like to join"}
       </Typography>
-      <form ref={form} onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={handleSubmit} style={{ display: 'none' }}>
         <div className={classes.inputContainer}>
           {!hasUsername && (
             <div className={classes.textFieldContainer}>
@@ -124,6 +133,7 @@ export default function RoomNameScreen({ name, roomName, setName, setRoomName, h
         </div>
         <Grid container justifyContent="flex-end">
           <Button
+            id="continue-button"
             variant="contained"
             type="submit"
             color="primary"
